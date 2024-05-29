5956fa613c6ed4f958755d22f5542b02f951a3a7
Pro-Nitish <70988892+Pro-Nitish@users.noreply.github.com>
2024-05-29 11:07:28 +1000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,12 @@
+// script.js
 document.addEventListener('DOMContentLoaded', function() {
-    AOS.init();  // Initialize AOS animation library
-
-    // You can add more interactive JavaScript code here
+    // Example: Smooth scrolling for navigation links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
 });
