0a1ebe5d23e4f6f6399f926a7c0cc40c900a1655
Nitish Ray <70988892+nitishrayofficial@users.noreply.github.com>
2024-06-13 23:17:07 +1000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-// script.js
 document.addEventListener('DOMContentLoaded', function() {
     // Example: Smooth scrolling for navigation links
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
